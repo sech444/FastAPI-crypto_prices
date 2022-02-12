fdec89b96b26a6a8c3292091e8faaa2b49a94d4d
sechmos <sechmos@localhost.localdomain>
2022-02-12 18:11:53 +0100

    initial commit

---
+++ app/main.py
@@ -0,0 +1,269 @@
+# -*- coding: utf-8 -*-
+#from datetime import datetime, timedelta
+#from .models import User, Gender, Role, List
+from uuid import uuid4
+from fastapi import Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+#from sqlalchemy.orm import Session
+#from .config import settings
+from fastapi import FastAPI, WebSocket, BackgroundTasks, APIRouter
+import json
+import requests
+from array import *
+
+
+app = FastAPI()
+
+'''
+db: List[User] = [
+    Users(
+    id= uuid4 (),
+    frist_name = "James",
+    last_name = "Jamila",
+    middle_name = None,
+    gender = Gender.female,
+    ),
+     User(
+    id= uuid4 (),
+    frist_name = "James",
+    last_name = "Jamila",
+    middle_name = None,
+    gender = Gender.female,
+    )
+]'''
+
+
+@app.get("/api/v1/users")
+async def fetch_users():
+    return db
+
+
+@app.get("/")
+def root():
+    return listData
+
+
+def get_price(crypto):
+    URL = 'https://www.bitstamp.net/api/ticker/btcusd/'
+    try:
+        r = requests.get(URL)
+        priceFloat = float(json.loads(r.text)['last'])
+        return priceFloat
+    except requests.ConnectionError:
+        print("Error querying Bitstamp API")
+
+
+def main():
+    last_price = -1
+
+    while True:
+
+        crypto = 'bitcoin'
+        price = get_price(crypto)
+
+        if price != last_price:
+           # print('Bitcoin price: ',price)
+            last_price = price
+    return
+
+
+@app.get('/btcusd')
+async def index(background_tasks: BackgroundTasks):
+    background_tasks.add_task(main)
+    return {
+        "coin": "BTC",
+        "name": "Bitcoin",
+        "rate": get_price("bitcoin"),
+        "coin_logo": "assets\/img\/xlm.png"
+    }
+
+
+def get_price_bch(crypto_bchusd):
+    URL = 'https://www.bitstamp.net/api/v2/ticker/bchusd/'
+    try:
+        r = requests.get(URL)
+        priceFloat = float(json.loads(r.text)['last'])
+        return priceFloat
+    except requests.ConnectionError:
+        print("Error querying Bitstamp API")
+
+
+def main_bch():
+    last_price = -1
+
+    while True:
+
+        crypto_bchusd = 'bitcoin_cash'
+        price = get_price_bch(crypto_bchusd)
+
+        if price != last_price:
+           # print('Bitcoin price: ',price)
+            last_price = price
+    return
+
+
+@app.get('/api/v1/bch')
+async def index_bch(background_tasks: BackgroundTasks):
+    background_tasks.add_task(main_bch)
+    return {
+        "coin": "BCH",
+        "name": "Bitcoin Cash",
+        "rate": get_price_bch("bitcoin_cash"),
+        "coin_logo": "assets\/img\/xlm.png"
+    }
+
+
+def getXlmucoinPrice(crypto_xlmusd):
+    URL = 'https://www.bitstamp.net/api/v2/ticker/xlmusd/'
+    try:
+        r = requests.get(URL)
+        priceFloat = float(json.loads(r.text)['last'])
+        return priceFloat
+    except requests.ConnectionError:
+        print("Error querying Bitstamp API")
+
+
+def main_xlmusd():
+    last_price = -1
+
+    while True:
+
+        crypto_xlmusd = 'xlmusd'
+        price = getXlmucoinPrice(crypto_xlmusd)
+
+        if price != last_price:
+           # print('Bitcoin price: ',price)
+            last_price = price
+    return
+
+
+@app.get('/api/v1/xlmusd')
+async def index_xlmusd(background_tasks: BackgroundTasks):
+    background_tasks.add_task(main_bch)
+    return {
+        "coin": "XLM",
+        "name": "Stellar",
+        "rate": getXlmucoinPrice("xlmusd"),
+        "coin_logo": "assets\/img\/xlm.png"
+    }
+
+
+def getLtccoinPrice(crypto_ltc):
+    URL = 'https://www.bitstamp.net/api/v2/ticker/ltcusd/'
+    try:
+        r = requests.get(URL)
+        priceFloat = float(json.loads(r.text)['last'])
+        return priceFloat
+    except requests.ConnectionError:
+        print("Error querying Bitstamp API")
+
+
+def main_ltc():
+    last_price = -1
+
+    while True:
+
+        crypto_ltc = 'litecoin'
+        price = getLtccoinPrice(crypto_ltc)
+
+        if price != last_price:
+           # print('Bitcoin price: ',price)
+            last_price = price
+    return
+
+
+@app.get('/api/v1/ltc')
+async def index_ltc(background_tasks: BackgroundTasks):
+    background_tasks.add_task(main_ltc)
+    return {
+        "coin": "ltc",
+        "name": "Litecoin",
+        "rate": getLtccoinPrice('litercoin'),
+        "coin_logo": "assets\/img\/xlm.png"
+    }
+
+
+
+def getDashcoinPrice(crypto_dash):
+    URL = 'https://www.dashcentral.org/api/v1/public'
+    try:
+        r = requests.get(URL)
+        priceFloat = json.loads(r.text)
+        #ans = json.loads(priceFloat)
+        # print(priceFloat)
+        data = (priceFloat['exchange_rates'])
+    except requests.ConnectionError:
+        print("Error querying Bitstamp API")
+    return data['dash_usd']
+# print('Dash:',getDashcoinPrice(crypto_dash))
+
+
+def main_dash():
+    last_price = -1
+
+    while True:
+
+        crypto_dash = 'dash'
+        price = getDashcoinPrice(crypto_dash)
+
+        if price != last_price:
+            #print('Dashcoin price: ',price)
+            last_price = price
+    return
+# print(main_xlmusd())
+
+
+@app.get('/api/v1/dash')
+async def index_dash(background_tasks: BackgroundTasks):
+    background_tasks.add_task(main_dash)
+    return {
+        "coin": "ltc",
+        "name": "Litecoin",
+        "rate": getDashcoinPrice("crypto_dash"),
+        "coin_logo": "assets\/img\/xlm.png"
+    }
+
+
+"""@app.get('/')
+async def index(background_tasks: BackgroundTasks):
+    background_tasks.add_task(main)
+    return {
+        "coin": "BTC",
+        "name": "Bitcoin",
+        "rate": get_price("bitcoin"),
+        "coin_logo": "assets\/img\/xlm.png
+    }  """
+
+
+listData = [
+    {
+        "coin": "btc",
+        "name": "Bitcoin",
+        "rate": get_price("bitcoin"),
+        "coin_logo": "assets\/img\/bch.png"
+    },
+    {
+        "coin": "bch",
+        "name": "Bitcoin Cash",
+        "rate": get_price_bch("bitcoin_cash"),
+        "coin_logo": "assets\/img\/bch.png"
+    },
+    {
+        "coin": "ltc",
+        "name": "Litecoin",
+        "rate": getLtccoinPrice('litercoin'),
+        "coin_logo": "assets\/img\/ltc.png"
+    },
+    {
+        "coin": "dash",
+        "name": "dash",
+        "rate": getDashcoinPrice("crypto_dash"),
+        "coin_logo": "assets\/img\/dash.png"
+    },
+    {
+        "coin": "xlm",
+        "name": "Stellar",
+        "rate": getXlmucoinPrice("stellar"),
+        "coin_logo": "assets\/img\/xlm.png"
+    }
+]
