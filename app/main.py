dc76eefb21096238b18f9345c3a0c9b0c6184d2b
sech444 <59539238+sech444@users.noreply.github.com>
2022-03-11 13:20:56 +0000

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,18 +1,31 @@
 # -*- coding: utf-8 -*-
 #from datetime import datetime, timedelta
-#from .models import User, Gender, Role, List
+from web3 import Web3, EthereumTesterProvider,HTTPProvider
 from uuid import uuid4
-from fastapi import Depends, status, HTTPException
+from fastapi.params import Body
+from pydantic import BaseModel 
 from fastapi.security import OAuth2PasswordBearer
 #from sqlalchemy.orm import Session
-#from .config import settings
-from fastapi import FastAPI, WebSocket, BackgroundTasks, APIRouter
+#from ethereumweb3 import Get_bals , send_transactions, transaction_receipt
+from fastapi import FastAPI, WebSocket, BackgroundTasks, APIRouter, Depends, status, HTTPException, Form
 import json
 import requests
 from array import *
 
+#webhook_url = "https://webhook.site/efac6181-8311-4cd1-86f3-c7a45f2b1a04"
+
+#app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
+# print("7dbec09e214cab2b4f77636cd082c65f85442d0ea65a59c28aa177158c4fe0c0")
+
+w3 = Web3(Web3.HTTPProvider('https://eth.getblock.io/rinkeby/?api_key=a8064b26-3884-47bb-92dc-5331a2213e3d'))
+#w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+
+abi = '[{"constant":true,"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"addedValue","type":"uint256"}],"name":"increaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"mint","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"subtractedValue","type":"uint256"}],"name":"decreaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"}]'
+
 
 app = FastAPI()
+
+#webhook_url = "https://webhook.site/36f9ce75-d60c-4e1c-b148-dee920cfca4f"
 
 '''
 db: List[User] = [
@@ -31,9 +44,13 @@ db: List[User] = [
     gender = Gender.female,
     )
 ]'''
+class Tx_user(BaseModel):
+    account_from: int = Body(None)
+    account_to: int = Body(None)
+    value_to_send: float = Body(None)
+    private_key: int = Body(None)
 
-
-@app.get("/list")
+@app.get("/")
 def root():
     return listData
 
@@ -178,7 +195,6 @@ async def index_ltc(background_tasks: BackgroundTasks):
     }
 
 
-
 def getDashcoinPrice(crypto_dash):
     URL = 'https://www.dashcentral.org/api/v1/public'
     try:
@@ -217,6 +233,68 @@ async def index_dash(background_tasks: BackgroundTasks):
         "rate": getDashcoinPrice("crypto_dash"),
         "coin_logo": "assets\/img\/xlm.png"
     }
+
+
+
+@app.post("/get_bals")
+async def Get_bals(background_tasks: BackgroundTasks,user_adr: str = Form(...)):
+    background_tasks.add_task(Get_bals(user_adr))
+    try:
+        _trans = w3.eth.get_balance(user_adr)
+        _bal2_ = w3.fromWei(_trans, 'ether')
+        return {"_bal2_": _bal2_}
+    except:
+        {'data':"Invalid_wallet" }
+
+
+
+
+@app.post("/api/tarnsaction")
+async def tarnsaction(background_tasks:BackgroundTasks,account_from:str = Form(...), account_to: str = Form(...),value_to_send: float=Form(...), private_key: str=Form(...)):
+    account_1 = account_from 
+    account_2 = account_to 
+    value    = value_to_send  
+                          
+    private_key = private_key  
+    addr = account_2
+     
+    nonce = w3.eth.getTransactionCount(account_1)
+
+    tx = {
+                    'nonce': nonce,
+                    'to': account_2,
+                    'value': w3.toWei(value, 'ether'),
+                    'gas': 200000,
+                    'gasPrice': w3.toWei('50', 'gwei'),
+                }
+        
+        
+
+
+    signed_tx = w3.eth.account.signTransaction(tx, private_key)
+    tx_hash =  w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+    new_data= (w3.toHex(tx_hash))
+    print(new_data)
+    return {"New_tarnsation": new_data , }
+   
+        
+
+@app.post('/api/tx_hash')
+async def transaction_receipt(background_tasks:BackgroundTasks,tx_hash:str = Form(...),webhook_url:str = Form(...)) -> dict():
+    
+    receipt_ = w3.eth.get_transaction_receipt(tx_hash)
+    w3.toJSON(receipt_ )
+    print(receipt_)
+    data = {
+        'acc': 'transaction',
+        'details': w3.toJSON(receipt_ ),
+    }
+    r=requests.post(webhook_url,data=json.dumps(data))
+
+    return {"data" : w3.toJSON(receipt_ )}
+
+   
+ 
 
 
 """@app.get('/')
